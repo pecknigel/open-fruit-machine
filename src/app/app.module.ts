ac1710b7994548e989dabc992fc38707ecdd08c2
Nigel Peck <nigel-git@nigelpeck.com>
2024-09-09 14:27:14 -0600

    Set up basic machine with reels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MachineComponent } from './components/machine/machine.component';
+import { MachineReelsComponent } from './components/machine-reels/machine-reels.component';
+import { MachineReelComponent } from './components/machine-reel/machine-reel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MachineComponent,
+    MachineReelsComponent,
+    MachineReelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/machine/machine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-machine',
+  templateUrl: './machine.component.html',
+  styleUrl: './machine.component.scss'
+})
+export class MachineComponent {
+
+}
